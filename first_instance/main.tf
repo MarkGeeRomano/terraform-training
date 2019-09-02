6319d471d02ea45e9a26915dea47697c8b5ee6ca
mark <markgromano31@gmail.com>
2019-09-01 22:27:41 -0400

    completed conditionals

--- first_instance/main.tf
+++ first_instance/main.tf
@@ -1,15 +1,16 @@
-resource "aws_instance" "test_instance" {
-  ami = "${var.name}"
+resource "aws_instance" "first_instance" {
+  ami = var.env == "prod" ? var.prod_ami : var.dev_ami
   instance_type = "${var.instance}"
-  count = "${length(var.name_count)}"
   tags = {
     Name = "myFirstInstance"
   }
 }
-
-output "instance" {
-  value = aws_instance.test_instance.*.ami
-}
-output "instance_type" {
-  value = aws_instance.test_instance.*.instance_type
-}
+# output "instance" {
+#   value = aws_instance.test_instance.*.tags.Name
+# }
+# output "instance_type" {
+#   value = aws_instance.test_instance.*.instance_type
+# }
+# output "instance_id" {
+#   value = "${join(",",aws_instance.test_instance.*.id)}"
+# }
--- first_instance/variable.tf
+++ first_instance/variable.tf
@@ -1,3 +1,11 @@
-variable "name" { default =  "ami-07d0cf3af28718ef8" }
+variable "ami_value" { 
+  type = "map"
+  default =  {
+    us_east_1 = "ami-07d0cf3af28718ef8" 
+  }
+}
 variable "instance" { default = "t2.micro" }
 variable "name_count" { default = ["s1", "s2", "s3"] }
+variable "env" { default = "dev" }
+variable "prod_ami" { default = "ami-07d0cf3af28718ef8" }
+variable "dev_ami" { default = "ami-0b898040803850657" }
